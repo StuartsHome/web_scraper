0a104a550ac49aea8b61dae1b541d2f56ad4854a
Stuart Smith <stuartsayswhat@googlemail.com>
2021-02-14 12:07:52 +0000

    need to allow TimeScale DB

--- search_page.py
+++ search_page.py
@@ -1,20 +1,45 @@
 from psaw import PushshiftAPI
-import datetime as dt 
+import datetime as dt
+import config
+import psycopg2
+import psycopg2.extras
 
+connection = psycopg2.connect(host=config.DB_HOST, database=config.DB_NAME, user=config.DB_USER, password=config.DB_PASS)
+# Dictionary cursor to loop through results as a dictionary
+cursor = connection.cursor(cursor_factory=psycopg2.extras.DictCursor)
+cursor.execute("""
+    SELECT * FROM stock
+""")
+rows = cursor.fetchall()
+print(rows)
 
-api = PushshiftAPI()
+# stocks = {}
+# for row in rows:
+#     stocks['$' + row['symbol']] = row['id']
 
-start_epoch = int(dt.datetime(2021, 1, 30).timestamp())
+# api = PushshiftAPI()
 
-submissions = list(api.search_submissions(after = start_epoch,
-                            subreddit='politics',
-                            filter = ['url', 'author', 'title', 'subreddit'],
-                            limit=10))
+# start_epoch = int(dt.datetime(2021, 2, 10).timestamp())
 
-for i,t in enumerate(submissions):
-    print(i, t)
-    print('#############')
+# """
+# Old Version using list
+# submissions = list(api.search_submissions(after = start_epoch,
+#                             subreddit='politics',
+#                             filter = ['url', 'author', 'title', 'subreddit'],
+#                             limit=10))
+# """
 
+# submissions = api.search_submissions(after = start_epoch,
+#                             subreddit = 'soccer',
+#                             filter = ['url', 'author', 'title', 'subreddit'])
+
+# for sub in submissions:
+#     words = sub.title.split()
+#     name_tags = list(set(filter(lambda word: word.lower().startswith('[optajose]'), words)))
+
+#     if len(name_tags) > 0:
+#         print(name_tags)
+#         print(sub.title)
 
 
 # send contents to file "python search_page.py >> <filename.txt>"
